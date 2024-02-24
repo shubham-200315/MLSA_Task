d83f1a8e258fdff4f9ff0546342928280248578a
Shubham Jaiswal <shubham.2125csit1070@kiet.edu>
2024-02-24 14:17:45 +0530

    added change

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,7 +21,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/forest-8531787_1280.jpeg";
+import image from "../images/forest-8531787_1280.jpg";
 
 const imageAltText = "Forest Image";
  
